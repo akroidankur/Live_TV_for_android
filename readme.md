8baf768a580824606cbd770f19546643224df51f
akroidankur <developerankurhazarika@gmail.com>
2024-10-10 21:55:54 +0530

    init

---
+++ readme.md
@@ -0,0 +1,10 @@
+change package.json name according to channel name
+change appid and appname in capacitor.config
+change name in ionic.config
+copy icons to resources folder
+npx cap add android
+npm run build
+npx capacitor-assets generate
+npx cap copy
+npx cap sync
+npx cap open android
---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { SplashScreen } from '@capacitor/splash-screen';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  constructor() {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    SplashScreen.show({
+      showDuration: 2000,
+      autoHide: true,
+    });
+  }
+}
